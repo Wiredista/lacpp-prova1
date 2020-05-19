24b41cb343fb899658aaa56a98db595b3acdb34c
Álvaro Antônio <wiredista@gmail.com>
2020-05-19 08:18:19 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -4,4 +4,11 @@ int main() {
 	cout << "SISTEMA DE PERFIL DE CLIENTE\n";
 	cout << "----------------------------\n";
 	cout << "INFORME OS DADOS DO ÚLTIMO ANO\n";
+
+	int compras;
+	float ticket;
+	cout << "Quantas compras o cliente fez no último ano? ";
+	cin >> compras;
+	cout << "Qual o ticket médio? ";
+	cin >> ticket;
 }
