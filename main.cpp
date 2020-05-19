b86dac6cad3b5a99be1d5c422b4b791bc6ca60f9
Álvaro Antônio <wiredista@gmail.com>
2020-05-19 07:46:11 -0300

    Cabecalho

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	cout << "SISTEMA DE PERFIL DE CLIENTE\n";
+	cout << "----------------------------\n";
+	cout << "INFORME OS DADOS DO ÚLTIMO ANO\n";
+}
